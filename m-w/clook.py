d4fbae37789f230c7c023fd6386a93f159be8ca7
Dez Moleski <dez@moleski.com>
2025-07-05 21:51:00 -0700

    change clook to use mw_tools.dictionary_request

--- m-w/clook.py
+++ m-w/clook.py
@@ -9,38 +9,16 @@
 # and does some very basic sanity-checks on the response. If the response
 # is JSON that parses to a Python list of dicts, then just print that out.
 #
-import json
-import os
-import requests
 import sys
 
-import keys
+import mw_tools
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         exit("Usage: clook <word>")
 
     word = sys.argv[1]
-    URL  = f'https://dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={keys.MW_COLLEGIATE}'
     
-    print(f'Looking up: "{word}" in Merriam-Webster\'s Collegiate® Dictionary', file=sys.stderr, flush=True)
-    
-    response = requests.get(URL)
-    if "Invalid API key" in response.text:
-        print(f'ERROR: Invalid key. Check key={keys.MW_COLLEGIATE} is correct for API https://dictionaryapi.com/api/v3/references/collegiate/json/',
-              file=sys.stderr, flush=True)
-    elif response:  # Evaluates to True for status codes < 400
-        #print(f'Request succeeded with status code: {response.status_code}', file=sys.stderr, flush=True)
-        data = response.json()
-        if not isinstance(data, list):
-            exit(f'ERROR: returned data is not a list')
-        if isinstance(data[0], str):
-            exit(f'ERROR: "{word}" NOT FOUND: did you mean any of these?\n{data}')
-        if not isinstance(data[0], dict):
-            exit(f'ERROR: returned data[0] is not a dict')
-        if data[0]['meta']['src'] != "collegiate":
-            exit(f'ERROR: returned data is not from expected source')
-        
-        print(response.text)
-    else:
-        print(f'Request failed with status code: {response.status_code}', file=sys.stderr, flush=True)
+    print(f'Looking up: "{word}" in {mw_tools.PRODUCT_DICTIONARY}', file=sys.stderr, flush=True)
+
+    mw_tools.dictionary_request(word)
